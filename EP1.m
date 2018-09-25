5e22d257ecccf00c87f1afb6b537b64c332b7187
Leandro Jabur Aumada <43447648+leandORaumada@users.noreply.github.com>
2018-09-25 11:29:44 -0300

    Corrente e Resistencia
    Implementação do Calculo da Corrente e Resistência;
    Adição de comentários no código;
    Mudança do campo e densidade de corrente para o SI;
    Alteração nos valores de iterações e resolução

---
+++ EP1.m
@@ -0,0 +1,100 @@
+
+
+linhasParametro = 9
+resolucao = 0.05
+sigma = (3.2) * 10^-3 %Condutividade Elétrica do Meio
+eps = 1.9 * (8.8541878117 * 10^(-12)) %Permissividade Eletrica do Meio
+
+a = 11
+b = 7
+c = 4
+d = b - 3
+g = 4
+h = (b-d)/2
+dx = resolucao
+dy = resolucao
+nx = a/dx
+ny = b/dy
+iteracoes = 100000
+corrente = 0
+espessura = 1
+
+
+iCondI = ((b-(d+h))/dy)+1
+iCondF = ((b-h)/dy)+1
+jCondI = (g/dx)+1
+jCondF = ((g+c)/dx)+1
+
+A = zeros (ny + 1,nx + 1);
+
+%Calculo da Matriz de Potenciais
+for i = iCondI:iCondF
+  for j = jCondI:jCondF
+    A(i,j) = 100;
+  end
+end
+for n = 0:iteracoes
+  for i = 2: ny
+    for j = 2: nx
+      if (~(i >= iCondI && i <= iCondF && j >= jCondI && j <= jCondF))
+        A (i,j) = (A(i-1,j) + A(i+1,j) + A(i,j-1) + A(i,j+1))/4;
+      end
+    end
+  end
+end
+contour (A,'LevelStep',10, 'ShowText','on')
+A
+
+
+%Calculo da Matriz de Campo
+eX = zeros (ny+2,nx+2);
+eY = zeros (ny+2,nx+2);
+for i = 1:ny
+    for j = 1:nx
+        eX(i+1,j+1)= (A(i+1,j) + A(i,j) - A(i+1,j+1) - A(i,j+1))/ (2*dx*10^-2);
+        eY(i+1,j+1)= (A(i,j+1) + A(i,j) - A(i+1,j+1) - A(i+1,j))/(2*dy*10^-2);
+    end
+end
+
+for i = 1:ny+2
+    eX(i,1)= eX(i,2);
+    eX(i,nx+2)=eX(i,nx+1);
+end
+for j = 1:nx+2
+    eY(1,j)= eY(2,j);
+    eY(ny+2,j)=eY(ny+1,j);
+end
+
+hold on
+u = jCondI:linhasParametro:jCondF;
+t = iCondI*ones(size(u));
+s = (iCondF+0.00000001)*ones(size(u));
+
+v = iCondI:linhasParametro:iCondF;
+z = (jCondI)*ones(size(v));
+w = (jCondF+0.00000001)*ones(size(v));
+
+x = [u u z w];
+y = [t s v v];
+
+streamline(eX,eY,x,y)
+%quiver(eX,eY)
+
+%Definição da Matriz de Corrente
+jX = sigma * eX;
+jY = sigma * eY;
+
+%Corrente dado jX
+for i = 2:ny
+    corrente = corrente + dy*10^-2 * espessura * abs(jX(i,2));
+    corrente = corrente + dy*10^-2 * espessura * abs(jX(i,nx));
+end
+%Corrente dado jY
+for j = 2:nx
+    corrente = corrente + dx*10^-2 * espessura * abs(jY(2,i));
+    corrente = corrente + dx*10^-2 * espessura * abs(jY(ny,2));
+end
+
+corrente
+resistencia = 100 / corrente
+
